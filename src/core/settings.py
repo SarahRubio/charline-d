87bfab93c3d9c3fc0352d26f93642320d30bbf3e
SarahRubio <sarahrubio.pro@gmail.com>
2021-05-05 18:10:56 +0200

    change config for secret_key

--- src/core/settings.py
+++ src/core/settings.py
@@ -14,7 +14,7 @@ PUBLIC_ROOT = BASE_DIR.parent/'public'
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
