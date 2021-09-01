c7a70e87bacedaccb58637348d703fa60da1f7e5
SarahRubio <sarahrubio.pro@gmail.com>
2021-09-01 11:36:57 +0200

    add naked domain to allowed_hosts

--- src/core/settings.py
+++ src/core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'www.cduchadeau-avocats.fr']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'www.cduchadeau-avocats.fr', 'cduchadeau-avocats.fr']
 
 
 # Application definition
