fa7af4b8aa641957517a6073edb513f3ba84da3b
SarahRubio <sarahrubio.pro@gmail.com>
2021-09-01 13:12:11 +0200

    redirect naked domain

--- src/core/settings.py
+++ src/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "core.middleware.WwwRedirectMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
